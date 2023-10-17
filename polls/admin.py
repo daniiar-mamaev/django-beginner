a680fea5fa8a6b3c3192e18b82d6f799ee918b29
daniiar-mamaev <daniiar.mamaev@gmail.com>
2023-10-17 16:22:07 +0600

    First commit Project

---
+++ polls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+admin.site.register(Question)
+# admin.site.register(Choice)
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['question', 'choice_text', 'votes']
+    search_fields = ['choice_text', 'question__question_text', ]
